8393c6ce83e7a9ad7fed19afa3742f6778394cba
Jsunitsch <jsunitsch@gmail.com>
2015-09-08 14:59:19 +0200

    Scripts
    Included rotation and movement in camera direction in Player and camera
    script

--- Assets/Script/CameraControl.cs
+++ Assets/Script/CameraControl.cs
@@ -11,12 +11,22 @@ public class CameraControl : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		offset = transform.position - player.transform.position;
-
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
 		transform.position = player.transform.position + offset;
+
+		if (Input.GetKey ("q")) {
+			transform.RotateAround (player.transform.position, Vector3.up, 0.5f);
+			offset = transform.position - player.transform.position;
+
+		} else if (Input.GetKey ("e")) {
+			transform.RotateAround (player.transform.position, Vector3.down, 0.5f);
+			offset = transform.position - player.transform.position;
+		}
+
 
 	}
 }
--- Assets/Script/PlayerControl.cs
+++ Assets/Script/PlayerControl.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class PlayerControl : MonoBehaviour {
 
+	public GameObject my_cam;
 	public float speed;
 	private Rigidbody rb;
 	// Use this for initialization
@@ -14,13 +15,10 @@ public class PlayerControl : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
-		float moveLR = Input.GetAxis ("Horizontal");
-		float moveUD = Input.GetAxis ("Vertical");
 
-		Vector3 move = new Vector3 (moveLR, 0.0f, moveUD);
-
-		rb.AddForce (move * speed);
+		Vector3 controlDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+		Vector3 actualDirection = my_cam.transform.TransformDirection(controlDirection);
+		rb.AddForce ((actualDirection * speed) );
 	}
 
 	public void FreezeControl(){
